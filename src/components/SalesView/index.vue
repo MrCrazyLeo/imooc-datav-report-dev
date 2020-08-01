dff38bcac2912cd2654c58a2aef3a87f8a8f8a3e
MrCrazyLeo <641512874@qq.com>
2020-08-01 22:48:03 +0800

    sales-view组件头部开发

--- src/components/SalesView/index.vue
+++ src/components/SalesView/index.vue
@@ -2,7 +2,42 @@
   <div class="sales-view">
     <el-card shadow="hover">
       <template v-slot:header>
-        <div>111222</div>
+        <div class="menu-wrapper">
+          <el-menu
+            mode="horizontal"
+            :default-active="activeIndex"
+            @select="handleSelect"
+            class="sales-view-el-menu"
+          >
+            <el-menu-item index="1">销售额</el-menu-item>
+            <el-menu-item index="2">访问量</el-menu-item>
+          </el-menu>
+          <div class="menu-right">
+            <el-radio-group
+              v-model="radioSelect"
+              size="small"
+            >
+              <el-radio-button label="今日"></el-radio-button>
+              <el-radio-button label="本周"></el-radio-button>
+              <el-radio-button label="本月"></el-radio-button>
+              <el-radio-button label="本年"></el-radio-button>
+            </el-radio-group>
+            <el-date-picker
+              v-model="date"
+              type="daterange"
+              range-separator="至"
+              start-placeholder="开始日期"
+              end-placeholder="结束日期"
+              size="small"
+              :picker-options="pickerOptions"
+              unlink-panels
+              class="sales-view-date-picker"
+            ></el-date-picker>
+          </div>
+        </div>
+      </template>
+      <template>
+        1111
       </template>
     </el-card>
   </div>
@@ -13,18 +48,46 @@ export default {
   name: 'SalesView',
   data() {
     return {
-      data: {
-        xAxis: {
-          type: 'category'
-        },
-        yAxis: {},
-        series: [
+      activeIndex: '1',
+      radioSelect: '今日',
+      date: '',
+      pickerOptions: {
+        shortcuts: [
           {
-            type: 'line',
-            data: [100, 200, 300, 400]
+            text: '最近一周',
+            onClick(picker) {
+              const end = new Date()
+              const start = new Date()
+              start.setTime(start.getTime() - 3600 * 1000 * 24 * 7)
+              picker.$emit('pick', [start, end])
+            }
+          },
+          {
+            text: '最近一个月',
+            onClick(picker) {
+              const end = new Date()
+              const start = new Date()
+              start.setTime(start.getTime() - 3600 * 1000 * 24 * 90)
+              picker.$emit('pick', [start, end])
+            }
+          },
+          {
+            text: '最近三个月',
+            onClick(picker) {
+              const end = new Date()
+              const start = new Date()
+              start.setTime(start.getTime() - 3600 * 1000 * 24 * 90)
+              picker.$emit('pick', [start, end])
+            }
           }
         ]
       }
+    }
+  },
+  methods: {
+    handleSelect(index) {
+      this.activeIndex = index
+      console.log(index)
     }
   }
 }
@@ -33,5 +96,30 @@ export default {
 <style lang='scss' scoped>
 .sales-view {
   margin-top: 20px;
+  .menu-wrapper {
+    position: relative;
+    display: flex;
+    .sales-view-el-menu {
+      width: 100%;
+      padding-left: 20px;
+      .el-menu-item {
+        height: 50px;
+        line-height: 50px;
+        margin: 0 20px;
+      }
+    }
+    .menu-right {
+      position: absolute;
+      right: 20px;
+      top: 0;
+      display: flex;
+      height: 50px;
+      align-items: center;
+      justify-content: flex-end;
+      .sales-view-date-picker {
+        margin-left: 20px;
+      }
+    }
+  }
 }
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
-import { Card, Row, Col } from 'element-ui'
+import { Card, Row, Col, Menu, MenuItem, RadioGroup, RadioButton, DatePicker } from 'element-ui'
 
 Vue.use(Card)
 Vue.use(Row)
 Vue.use(Col)
+Vue.use(Menu)
+Vue.use(MenuItem)
+Vue.use(RadioGroup)
+Vue.use(RadioButton)
+Vue.use(DatePicker)
